4d172aafc2be92f5000efbf7afec0734f69e9ad3
Arham Khawar <68909415+ch-Arham@users.noreply.github.com>
2022-07-16 22:48:49 +0500

    first commit

---
+++ tutorial.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// main is the entry point for the application.
+func main() {
+	// Println will print to stdout.
+	fmt.Println("Hello World!") // single quotes for characters only
+}
